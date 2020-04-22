3ab3de96e14b2dc9e683ae2999deb458703be9d4
goN <acorelado@gmail.com>
2020-04-22 13:12:32 -0300

    some interface improvements

--- lib/main.dart
+++ lib/main.dart
@@ -69,6 +69,7 @@ class _HomeState extends State<Home> {
                   dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
                   euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
                   return SingleChildScrollView(
+                    padding: EdgeInsets.all(10.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: <Widget>[
@@ -77,11 +78,11 @@ class _HomeState extends State<Home> {
                           size: 150.0,
                           color: Colors.amber,
                         ),
-                        TextField(
-                          decoration: InputDecoration(
-                              labelText: "Reais",
-                              labelStyle: TextStyle(color: Colors.amber)),
-                        )
+                        buildTextField("Reais", "R\$"),
+                        Divider(),
+                        buildTextField("Dólares", "US\$"),
+                        Divider(),
+                        buildTextField("Euros", "€")
                       ],
                     ),
                   );
@@ -90,4 +91,14 @@ class _HomeState extends State<Home> {
           }),
     );
   }
+}
+
+Widget buildTextField(String label, String prefix) {
+  return TextField(
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.amber),
+        prefixText: prefix),
+    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+  );
 }
